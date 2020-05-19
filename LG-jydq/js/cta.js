3224b914e3fd237db8ae917898e006cc87c2dea8
Zhao ping ping <1361334489@qq.com>
2020-05-19 15:39:11 +0800

    新建基建模板

---
+++ LG-jydq/js/cta.js
@@ -0,0 +1,30 @@
+// //动态设置html的font-size
+// window.onresize=r;
+// function r(){
+// 	var winw=window.innerWidth
+// 	document.getElementsByTagName("html")[0]
+// 	.style.fontSize=winw*0.084745+"px"
+// }
+// r()
+
+//banner插件
+    var swiper = new Swiper('.swiper-container', {
+      cssMode: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination'
+      },
+      mousewheel: true,
+      keyboard: true,
+    });
+	
+
+$(document).ready(function(){
+	
+	
+})
+
+
